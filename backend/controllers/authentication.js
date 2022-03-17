bd475be5a6969fab5821ce8635f04694beed62f3
unknown <derek.slauson5@gmail.com>
2022-03-16 20:34:14 -0500

    Finished activity

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -14,8 +14,32 @@ router.post("/", async (req, res) => {
             message: `Could not find a user with the provided email and password`
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
 })
+
+router.get("/profile", async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+// router.post("/super-important-route", async (req, res) => {
+//     if(req.session.userId) {
+//         console.log("Do really important thing")
+//         res.send("Done")
+//     } else {
+//         console.log("You're not authorized")
+//         res.send("Denied")
+//     }
+// })
 
 module.exports = router
