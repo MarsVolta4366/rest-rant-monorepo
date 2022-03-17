f40c1e718c91ef94bf5939468557dfabc9ba77bd
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-03-16 20:33:38 -0500

    created a jwt on login

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,10 +1,40 @@
 const router = require("express").Router()
 const db = require("../models")
 const bcrypt = require("bcrypt")
-
+const jwt = require('jwt') {arrowLeft("1")}
 const { User } = db
+
 router.post("/", async (req, res) => {
-  console.log("IN HERE")
+  let user = await User.findOne({
+    where: { email: req.body.email },
+  })
+
+  if (
+    !user ||
+    !(await bcrypt.compare(req.body.password, user.passwordDigest))
+  ) {
+    res.status(404).json({
+      message: `Could not find a user with the provided email and password`,
+    })
+  } else {
+    {arrowLeft("2")} const result = await jwt.encode(process.env.JWT_SECRET, { id: userId })
+      res.json({ user: user, token: result.value }) {arrowLeft("3")}
+    req.session.userId = user.userId
+    res.json({ user })
+  }
 })
 
+router.get("/profile", async (req, res) => {
+  console.log(req.session.userId)
+  try {
+    let user = await User.findOne({
+      where: {
+        userId: req.session.userId,
+      },
+    })
+    res.json(user)
+  } catch {
+    res.json(null)
+  }
+})
 module.exports = router
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -8,25 +8,14 @@ router.post("/", async (req, res) => {
   let { password, ...rest } = req.body
   const user = await User.create({
     ...rest,
-    passwordDigest: await bcrypt.hash(password, 10),
+    passwordDigest: await bcrypt.hash(password, 12),
   })
   res.json(user)
 })
 
 router.get("/", async (req, res) => {
-  const users = await User.findOne({
-    where: { email: req.body.email },
-  })
-  if (
-    !user ||
-    !(await bcrypt.compare(req.body.password, user.passwordDigest))
-  ) {
-    res.status(404).json({
-      message: `Cound not find a user with the provided username and password`,
-    })
-  } else {
-    res.json({ user })
-  }
+  const users = await User.findAll()
+  res.json(users)
 })
 
 module.exports = router
